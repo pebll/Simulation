b89fd6c3b7c80c95c9c016a15fa8924a8785d8e4
Georg Pernice <georgpernice@gmail.com>
2022-07-09 16:38:42 +0200

    particle bewegen sich schon

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-from fieldmanager import Fieldmanager
+
 import pyglet
-import graphics
-from particle import Particle
+from particlemanager import Particlemanager
+from fieldmanager import Fieldmanager
 import math as m
 def velocityAt(x,y): #definiert das geschwindigkeitsfeld. das wäre das was wir in ströle errechnen täten
-    #return(1,0.5)
-    return (x,y)
-    return (x , m.sin(x))
-    return (0.2*m.pow(x-4,2) + 0.2*m.pow(y-4,2),  0.2*m.pow(y-4,2))#0.2*m.pow(y-4,2))
+    #return(1*m.pow(x,2),0.5)
+    #return (x,y)
+    #return (x , m.sin(x))
+    #return (0.2*m.pow(x-4,2) + 0.2*m.pow(y-4,2),  0.2*m.pow(y-4,2))#0.2*m.pow(y-4,2))
     return (m.sqrt(2 - 0.02*m.pow(y,2)),m.sqrt(2 - 0.02*m.pow(y,2)))
 
 window = pyglet.window.Window(fullscreen=False)
-
+clock = pyglet.clock
 batchOfArrows = pyglet.graphics.Batch()
-field = Fieldmanager(10, batchOfArrows, velocityAt)
+batchOfParticles = pyglet.graphics.Batch()
+field = Fieldmanager(20, 25 ,batchOfArrows, velocityAt)
+particlepositions = ((0,0),(10,200),(40,40))
+particleswarm = Particlemanager(particlepositions,batchOfParticles,field)
 
-
-#particule = pyglet.resource.image('particle.png') #deleten?
-particle1 = Particle()
-
-
-
-    
+  
 
 field.updateArrows()
+pyglet.clock.schedule(particleswarm.update)
 @window.event
 def on_draw():
+    dt = clock.tick()
+    #particleswarm.update(dt)
     window.clear()
     
     batchOfArrows.draw()
-    particle1.image.draw()
+    #pyglet.sprite.Sprite(pyglet.image.load("particle.png"),0,0).draw()
+    batchOfParticles.draw()
+    
     
     
 
